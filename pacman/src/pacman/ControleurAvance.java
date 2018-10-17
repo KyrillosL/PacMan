3cc5719ed98cc9b13ca7be3e5983841f40901541
Cyril Lepinette <lepinette.cyril@gmail.com>
2018-10-17 11:03:33 +0200

    Add class

---
+++ pacman/src/pacman/ControleurAvance.java
@@ -0,0 +1,56 @@
+package pacman;
+
+public class ControleurAvance implements ControleurGame {
+
+
+	Game game; 
+	View view; 
+	
+	public ControleurAvance(Game g) {
+		game = g; 
+		view = new View(this);
+	}
+	
+	@Override
+	public void launch() {
+		// TODO Auto-generated method stub
+		game.launch(); 
+		view.run.setEnabled(false);
+		view.pause.setEnabled(true);
+		view.restart.setEnabled(true);
+
+	}
+	
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		game.init(); 
+		view.restart.setEnabled(false);
+		view.pause.setEnabled(true);
+
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		game.stop(); 
+		view.pause.setEnabled(false);
+		view.run.setEnabled(true);
+
+	}
+	
+	@Override
+	public void step() {
+		// TODO Auto-generated method stub
+		try {
+			game.step();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+
+	}
+
+}
+
+
