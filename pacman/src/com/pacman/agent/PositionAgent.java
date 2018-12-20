13b77a3098121c64e664d3f7e4d8d248e11c208a
Samsonov366 <thelem@hotmail.fr>
2018-12-20 22:36:09 +0100

    remis coorEgales et getvoisins dans PositionAgent

--- pacman/src/com/pacman/agent/PositionAgent.java
+++ pacman/src/com/pacman/agent/PositionAgent.java
@@ -1,4 +1,9 @@
 package com.pacman.agent;
+
+import java.util.ArrayList;
+
+import com.pacman.vue.Maze;
+
 public class PositionAgent {
 	
 	private int x;
@@ -35,5 +40,25 @@ public class PositionAgent {
 	public void setDir(int dir) {
 		this.dir = dir;
 	}
+	public boolean coordonneesEgales(PositionAgent posAgent) {
+		return (this.x==posAgent.x)&&(this.y==posAgent.y);
+		
+	}
+	public ArrayList<PositionAgent> getVoisins(Maze maze){
+		ArrayList<PositionAgent> voisins = new ArrayList<>();
+		
+				//test si coordonnée dans labyrinthe et si ce n'est pas un mur--> si case accessible
+				if(( this.x-1 >= 0 ) && ( this.y>=0 ) && ( this.x-1 < maze.getSizeX() ) && ( this.y < maze.getSizeY()) && ( !maze.isWall(this.x-1, this.y) ))	
+					voisins.add(new PositionAgent(this.x-1, this.y, this.dir));
+				if(( this.x >= 0 ) && ( this.y-1>=0 ) && ( this.x < maze.getSizeX() ) && ( this.y-1 < maze.getSizeY()) && ( !maze.isWall(this.x, this.y-1) ))	
+					voisins.add(new PositionAgent(this.x, this.y-1, this.dir));
+				if(( this.x+1 >= 0 ) && ( this.y>=0 ) && ( this.x+1-1 < maze.getSizeX() ) && ( this.y < maze.getSizeY()) && ( !maze.isWall(this.x+1, this.y) ))	
+					voisins.add(new PositionAgent(this.x+1, this.y, this.dir));
+				if(( this.x >= 0 ) && ( this.y+1 >=0 ) && ( this.x < maze.getSizeX() ) && ( this.y+1 < maze.getSizeY()) && ( !maze.isWall(this.x, this.y+1) ))	
+					voisins.add(new PositionAgent(this.x, this.y+1, this.dir));
+			
+		return voisins;
+		
+}
 	
 }
