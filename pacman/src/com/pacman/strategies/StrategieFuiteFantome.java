082947d8d92cd2ff36df78a10936a84c3fbcb20f
Samsonov366 <thelem@hotmail.fr>
2018-12-21 17:03:48 +0100

    che pas

---
+++ pacman/src/com/pacman/strategies/StrategieFuiteFantome.java
@@ -0,0 +1,16 @@
+package com.pacman.strategies;
+
+import com.pacman.agent.Agent;
+import com.pacman.agent.AgentAction;
+import com.pacman.vue.Maze;
+
+public class StrategieFuiteFantome implements Strategie{
+
+	@Override
+	public AgentAction getAction(Agent a, Maze m) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
